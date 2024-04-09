66a47c7003ffb911d310ecc7f2c77f1c245264d8
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2024-04-09 12:14:07 +0200

    fix doc

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ import os
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../deepdespeckling'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
